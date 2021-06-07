2a7402ac301f089b357bfdf45cf4acb47929a870
Juliana Lim <julianalim@Julianas-MacBook-Air.local>
2021-06-07 00:51:30 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>  s u p </h1>
-      </header>
+    <div className="App"> 
+        <header>
+          <button class="Button"> About </button>
+          <button class="Button"> Bio </button>
+          <button class="Button"> Background </button>
+          <button class="Button"> Work </button>
+        </header>
+        <header className = "App-header">
+          <button class="DogButton"> </button>
+          <button class="DesignButton"> </button>
+          <button class="ProjButton"> </button>
+        </header>
     </div>
   );
 }
